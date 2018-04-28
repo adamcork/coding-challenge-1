c4a264c4173a7f012ae26bbf8269e8c8a1b67506
Adam Cork <adamcork@gmail.com>
2018-04-28 22:37:30 +0100

    Passing first test, returning correctly for a single forward operation.

--- ships-api/Services/NavigationService.cs
+++ ships-api/Services/NavigationService.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace ships_api.Services
 {
+    struct ShipPosition
+    {
+        public Int16 XCoordinate;
+        public Int16 YCoordinate;
+        public char Direction;
+    }
     public class NavigationService
     {
         private Int16 _gridXMaximum;
@@ -15,7 +21,51 @@ namespace ships_api.Services
 
         public string ProcessShipInstructions(string startingPosition, string navigationalSequence)
         {
-            throw new NotImplementedException();
+            var currentPosition = SplitPositionString(startingPosition);
+            foreach(char c in navigationalSequence)
+            {
+                currentPosition = ApplyTransition(currentPosition, c);
+            }
+
+            return $"{currentPosition.XCoordinate} {currentPosition.YCoordinate} {currentPosition.Direction}";
+        }
+
+        private ShipPosition ApplyTransition(ShipPosition currentPosition, char navigationalOperation)
+        {
+            if(navigationalOperation == 'F')
+            {
+                switch(currentPosition.Direction)
+                {
+                    case 'N':
+                        currentPosition.YCoordinate++;
+                        break;
+                    case 'E':
+                        currentPosition.XCoordinate++;
+                        break;
+                    case 'S':
+                        currentPosition.YCoordinate--;
+                        break;
+                    case 'W':
+                        currentPosition.XCoordinate--;
+                        break;                    
+                }
+            }
+
+            return currentPosition;
+        }
+
+        private ShipPosition SplitPositionString(string position)
+        {
+            // TODO: Expand to handle any whitespace as per spec.
+            var parts = position.Split(' '); 
+            var shipPosition = new ShipPosition();
+
+            // TODO: Add handling for invalid input
+            shipPosition.XCoordinate = Int16.Parse(parts[0]); 
+            shipPosition.YCoordinate = Int16.Parse(parts[1]);
+            shipPosition.Direction = parts[2][0];
+
+            return shipPosition;
         }
     }
 }
