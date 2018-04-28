5bf5fd1f759a6c84dee4ab6a99339c8a5d29bd0a
Adam Cork <adamcork@gmail.com>
2018-04-28 22:44:04 +0100

    Added right turn test, currently failing.

--- ships-api-tests/NavigationServiceTests.cs
+++ ships-api-tests/NavigationServiceTests.cs
@@ -23,5 +23,14 @@ namespace ships_api_tests
 
             Assert.Equal("1 1 N", response);
         }
+
+        [Fact]
+        public void CanNavigateRight()
+        {
+            var sut = new NavigationService(5, 3);
+            var response = sut.ProcessShipInstructions("1 1 E", "R");
+
+            Assert.Equal("1 1 S", response);
+        }
     }
 }
